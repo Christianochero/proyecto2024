73a12066c89fd4429ff06ae23ddb74eeb3320f1e
Christianochero <158649690+Christianochero@users.noreply.github.com>
2024-02-02 21:11:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-
 import streamlit as st 
-
 import pandas as pd 
-
 import numpy as np
 
-
-
-st.title('UPC Data Science 2024')
-
+st.title('UPC Data Science 2024 - Jesus Christian Osorio Chero')
 st.header('Simulador Ventas')
-
 n = st.slider("cant. ventas", 5,100, step=1)
-
 chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
-
 st.line_chart(chart_data)
 
 
