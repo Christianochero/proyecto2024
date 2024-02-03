6cb1f75fe0447eb81579562599ff8d6d6e2595bb
Christianochero <158649690+Christianochero@users.noreply.github.com>
2024-02-02 20:50:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import pandas as pd
 import numpy as np
 
 st.title('Titulo del prouyecto')
+st.header('Simuladro de ventas')
+n = st.slider("n", 5,100, step=1)
+
+chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
+
+st.line_chart(chart_data)
+
+
+
