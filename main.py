c8155cb205c58cb8a13fb28399f1c332794606a8
Christianochero <158649690+Christianochero@users.noreply.github.com>
2024-02-02 21:04:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import streamlit as st 
 
-import pandas as pd 
-
-import numpy as np 
-
-st.title('Titulo') 
-
-PETER MONTALVO GARCIA20:43
-main.py
-
-PETER MONTALVO GARCIA20:47
-n = st.slider("n", 5,100, step=1)
-
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-
-st.line_chart(chart_data)
-
-PETER MONTALVO GARCIA20:49
 import streamlit as st 
 
 import pandas as pd 
